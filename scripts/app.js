4c767b24abf5a19c5fcdca6f7b99eee7b5ff3c6e
KarterDreamzyMoore <kmoore3365@myhta.org>
2018-03-29 14:32:00 -1000

    whack-a-mole

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,18 @@
-
 window.onload = function(){
 
     var dirt = document.getElementsByClassName('dirt');
-
+    var lose = document.getElementById("lose");
     var sound = document.getElementById("sound");
     var grid = document.getElementById("dirtbox");
-
-    var score = document.getElementById("score");
-
-    var realScore = new Number();
     
 
     for(let i = 0; i < dirt.length; i++){
         dirt[i].addEventListener("click", function(){
                 if (dirt[i].innerHTML){
                     sound.play();
-                    realScore += 1;
-                    score.innerHTML = realScore;
                     dirt[i].innerHTML = "";
                 } else {
+                    lose.play(); n
                     console.log("nothing is in there");
                 }
             });
@@ -26,19 +20,13 @@ window.onload = function(){
 
 
 setInterval(function setMole(){
-
-    var mole = document.createElement("img");
-    mole.src = "assets/mole.png"
-
+    var mole = document.createElement("div");
     mole.setAttribute("id", "mole");
     var randInt = Math.floor(Math.random() * Math.floor(dirt.length));
-
     if (dirt[randInt].innerHTML === ""){
         dirt[randInt].appendChild(mole);
     } else {
         console.log("There are too many moles");
     }
-
 },3000)};
-
 
